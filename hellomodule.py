4b7281ff85fa7b1b69b80077a30a81331377d850
Lucky2245 <95869708+Lucky2245@users.noreply.github.com>
2022-08-27 17:20:31 -0400

    Update hellomodule.py

--- hellomodule.py
+++ hellomodule.py
@@ -1,6 +1,34 @@
 def say_hello():
-  print("Hello, World!")
-def paragraph():
-  print("<p></p>")
+  message = "Hello, World!" #code will be say_hello()
+  print("The file will be saved as index.html")
+  f = open("index.html", "w")
+  f.write(message)
+  f.close()
 def noscript():
-  print("<noscript>Please Enable Javascript</noscript>")
+   message = "<noscript>Please Enable Javascript</noscript>" #code will be noscript
+   print("The file will be saved as index.html")
+   f = open("index.html", "w")
+   f.write(message)
+   f.close()
+def style():
+  message = "<style>p{color:orange;}</style>" #code will be style()
+  print("The file will be saved as index.html")
+  f = open("index.html", "w")
+  f.write(message)
+  f.close()
+def headerone():
+  message = "<h1>Header One</h1>"#code will be headerone()
+  print("The file will be saved as index.html")
+  f = open("index.html", "w")
+  f.write(message)
+  f.close()
+def script():
+  message ="<script></script>"
+  print("The file will be saved as index.html")
+  f = open("index.html", "w")
+  f.write(message)
+  f.close()
+def run():
+  print("running The Code")
+  f = open("index.html", "r")
+  print(f.read())
