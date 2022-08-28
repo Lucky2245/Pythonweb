0a3afb05a8314ebc64182268270408a8483148f0
Lucky2245 <95869708+Lucky2245@users.noreply.github.com>
2022-08-28 14:24:02 -0400

    Update myfunctions.py

--- mypthonlib/myfunctions.py
+++ mypthonlib/myfunctions.py
@@ -1,1 +1,14 @@
+from math import *
 
+def haversine(lon1: float, lat1: float, lon2: float, lat2: flost) ->
+float:
+  #Convert decimal degrees radians
+  lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+  
+  #havarsine formula
+  dlon = lon1 - lon2
+  dlat = lat2 - lat1
+  a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+  c = 2* asin(sqrt(a))
+  r = 6371 # Radius of earth kilometers
+  return c*r
