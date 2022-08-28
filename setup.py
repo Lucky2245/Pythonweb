36133e90fef42f8bc59e2c63bfa19aae1aae3402
Lucky2245 <95869708+Lucky2245@users.noreply.github.com>
2022-08-27 20:35:23 -0400

    Delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-from distutils.extension import Extension
-
-setup(name="PackageName",
-    ext_modules=[
-      Extension("goodbye", ["goodbyemodule.cpp"],
-        libraries = ["boost_python"]
-    ])
