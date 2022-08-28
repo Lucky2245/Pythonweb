bfeca8b7cdb5b87454393cbd182b438cadef4034
Lucky2245 <95869708+Lucky2245@users.noreply.github.com>
2022-08-27 23:47:39 -0400

    Update hellomodule.py

--- hellomodule.py
+++ hellomodule.py
@@ -1,4 +1,4 @@
-def multiply(number, muiltiplier):
+ multiply(number, muiltiplier):
     return number * muiltiplier
 def add(number, adder):
     return number + adder
@@ -6,3 +6,5 @@ def subtract(number, subtracter):
     return number - subtracter
 def divide(number, divider):
     return number/divider
+def say(PRint):
+    print(PRint)
