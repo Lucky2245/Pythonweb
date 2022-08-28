fec63cbac39abd3c827741602e71bb367bfc7138
Lucky2245 <95869708+Lucky2245@users.noreply.github.com>
2022-08-28 14:59:47 -0400

    Update myfunctions.py

--- mypthonlib/myfunctions.py
+++ mypthonlib/myfunctions.py
@@ -5,7 +5,7 @@ float:
   #Convert decimal degrees radians
   lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
   
-  #havarsine formula
+  #heversine formula
   dlon = lon1 - lon2
   dlat = lat2 - lat1
   a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
