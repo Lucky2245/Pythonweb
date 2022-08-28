d4a64cc03a2db2ebf25e0802459c06e5d3f63186
Lucky2245 <95869708+Lucky2245@users.noreply.github.com>
2022-08-28 16:37:32 -0400

    Create test_functions.py

---
+++ pymath/test/test_functions.py
@@ -0,0 +1,7 @@
+from mypthonlib import functions
+def test_haversine():
+  assert myfunctions.haversine(52.3703, 4.288, 1.394, 39439.393) ==99483.39948
+  
+print(add(5,5))
+print(subtract(10,5))
+print(multiply(5,2))
