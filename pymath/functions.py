48d5a910191ca17e3e0f08b8e67295aeb6ef375a
Lucky2245 <95869708+Lucky2245@users.noreply.github.com>
2022-08-28 16:34:18 -0400

    Create functions.py

---
+++ pymath/functions.py
@@ -0,0 +1,22 @@
+from math import *
+
+def haversine(lon1: float, lat1: float, lon2: float, lat2: float) ->
+float:
+  #Convert decimal degrees radians
+  lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+  
+  #havsersine formula
+  dlon = lon1 - lon2
+  dlat = lat2 - lat1
+  a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+  c = 2* asin(sqrt(a))
+  r = 6371 # Radius of earth kilometers
+  return c*r
+def add(Add,ADD):
+  return ADD+Add
+def subtract(Subtract,SUBTRACT):
+  return Subtract-SUBTRACT
+def multiply(Multiply, MUltiply):
+  return Multiply * MUltiply
+def divide(Divide, DIvide):
+  return Divide/DIvide
