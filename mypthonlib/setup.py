c4add1d8e0bedfafa6f377ed153ddfa7e2ea7fc6
Lucky2245 <95869708+Lucky2245@users.noreply.github.com>
2022-08-28 14:41:42 -0400

    Create setup.py

---
+++ mypthonlib/setup.py
@@ -0,0 +1,11 @@
+from setuptools import *
+setup(
+  name='mypthonlib',
+  packages=find_packages(include=['mypythonlib']),
+  version='0.1.0',
+  author ='SaturnLang',
+  license='MIT',
+  install_requires = [],
+  setup_requires =['pytest-runner'],
+  test_require v=
+)
