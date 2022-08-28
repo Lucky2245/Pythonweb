460d4bb3ff73fcabb7394ad1e0a4a7ce426fb612
Lucky2245 <95869708+Lucky2245@users.noreply.github.com>
2022-08-28 19:51:43 -0400

    Update __init__.py

--- pymath/test/__init__.py
+++ pymath/test/__init__.py
@@ -1,3 +1,3 @@
 from mypthonlib import functions
 def test_haversine():
-  assert myfunctions.haversine(52.3703, 4.288, 1.394, 39439.393) ==99483.39948
+  assert functions.haversine(52.3703, 4.288, 1.394, 39439.393) ==99483.39948
