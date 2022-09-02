3b4fb093b07a2a530117b274ecc6c5af6e85a2ba
Lucky2245 <95869708+Lucky2245@users.noreply.github.com>
2022-09-01 23:33:00 -0400

    Update test_functions.py

--- pymath/test/test_functions.py
+++ pymath/test/test_functions.py
@@ -1,6 +1,6 @@
 from pymath import functions
 def test_haversine():
-  assert myfunctions.haversine(52.3703, 4.288, 1.394, 39439.393) ==99483.39948
+  assert functions.haversine(52.3703, 4.288, 1.394, 39439.393) ==99483.39948
   
 print(add(5,5))
 print(subtract(10,5))
