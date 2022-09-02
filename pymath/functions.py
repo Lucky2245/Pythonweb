b38adec59ab49a48fc17c57c6226c83c2873e875
Lucky2245 <95869708+Lucky2245@users.noreply.github.com>
2022-09-01 23:46:16 -0400

    Update functions.py

--- pymath/functions.py
+++ pymath/functions.py
@@ -23,3 +23,5 @@ def multiply(Multiply, MUltiply):
   return Multiply * MUltiply#multiply: Multiply and multiply
 def divide(Divide, DIvide):
   return Divide/DIvide#divide: Divide and DIvide
+def Math(MAth):
+  print(Math)
